d1a0932348bd7366d0381556cbd75ab63fca4af2
Kerry Buckley <kerryjbuckley@gmail.com>
2025-12-01 09:50:16 +0000

    Create new mix project for day 1

---
+++ 01_secret_entrance/lib/secret_entrance.ex
@@ -0,0 +1,18 @@
+defmodule SecretEntrance do
+  @moduledoc """
+  Documentation for `SecretEntrance`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SecretEntrance.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ 01_secret_entrance/test/secret_entrance_test.exs
@@ -0,0 +1,8 @@
+defmodule SecretEntranceTest do
+  use ExUnit.Case
+  doctest SecretEntrance
+
+  test "greets the world" do
+    assert SecretEntrance.hello() == :world
+  end
+end
