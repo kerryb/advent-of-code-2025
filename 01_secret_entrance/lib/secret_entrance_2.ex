408c353e8f7dc396fa01c4b070713c8a88e11786
Kerry Buckley <kerryjbuckley@gmail.com>
2025-12-01 18:53:25 +0000

    count the total number of times zero is reached or crossed

--- 01_secret_entrance/lib/secret_entrance_2.ex
+++ 01_secret_entrance/lib/secret_entrance_2.ex
@@ -8,6 +8,11 @@ defmodule SecretEntrance2 do
   end
 
   defp new_position(position, total, distance, direction) do
-    {Integer.mod(position + String.to_integer(distance) * direction, 100), total}
+    new_position = position + String.to_integer(distance) * direction
+    {Integer.mod(new_position, 100), total + zero_count(new_position)}
   end
+
+  defp zero_count(0), do: 1
+  defp zero_count(position) when position > 0, do: div(position, 100)
+  defp zero_count(position), do: div(abs(position), 100) + 1
 end
--- 01_secret_entrance/test/secret_entrance_2_test.exs
+++ 01_secret_entrance/test/secret_entrance_2_test.exs
@@ -10,12 +10,20 @@ defmodule SecretEntrance2Test do
       assert SecretEntrance2.turn(42, 0, "L32") == {10, 0}
     end
 
-    test "wraps from 99 to 0 when turning right" do
-      assert SecretEntrance2.turn(90, 0, "R20") == {10, 0}
+    test "wraps from 99 to 0, incrementing the total, when turning right" do
+      assert SecretEntrance2.turn(90, 0, "R20") == {10, 1}
     end
 
-    test "wraps from 0 to 99 when turning left" do
-      assert SecretEntrance2.turn(10, 0, "L20") == {90, 0}
+    test "increments the total when landing on zero" do
+      assert SecretEntrance2.turn(90, 0, "R10") == {0, 1}
+    end
+
+    test "increments the total once per revolution when turning right by more than 100" do
+      assert SecretEntrance2.turn(90, 0, "R220") == {10, 3}
+    end
+
+    test "increments the total once per revolution when turning left by more than 100" do
+      assert SecretEntrance2.turn(10, 0, "L220") == {90, 3}
     end
   end
 end
