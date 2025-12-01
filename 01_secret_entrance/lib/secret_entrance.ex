95bbf19fcd4f010fe472801be3543186ae36ae7c
Kerry Buckley <kerryjbuckley@gmail.com>
2025-12-01 10:08:22 +0000

    Collapse short functions to simgle line

--- 01_secret_entrance/lib/secret_entrance.ex
+++ 01_secret_entrance/lib/secret_entrance.ex
@@ -1,11 +1,6 @@
 defmodule SecretEntrance do
-  def turn(position, <<"R", count::binary>>) do
-    new_position(position, count, 1)
-  end
-
-  def turn(position, <<"L", count::binary>>) do
-    new_position(position, count, -1)
-  end
+  def turn(position, <<"R", count::binary>>), do: new_position(position, count, 1)
+  def turn(position, <<"L", count::binary>>), do: new_position(position, count, -1)
 
   defp new_position(position, count, direction) do
     Integer.mod(position + String.to_integer(count) * direction, 100)
