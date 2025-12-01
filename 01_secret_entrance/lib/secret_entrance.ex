b037f0ceac791ce97ddd34d41a617418fa540615
Kerry Buckley <kerryjbuckley@gmail.com>
2025-12-01 09:59:09 +0000

    Count turns to the left

--- 01_secret_entrance/lib/secret_entrance.ex
+++ 01_secret_entrance/lib/secret_entrance.ex
@@ -2,4 +2,8 @@ defmodule SecretEntrance do
   def turn(position, <<"R", count::binary>>) do
     position + String.to_integer(count)
   end
+
+  def turn(position, <<"L", count::binary>>) do
+    position - String.to_integer(count)
+  end
 end
--- 01_secret_entrance/test/secret_entrance_test.exs
+++ 01_secret_entrance/test/secret_entrance_test.exs
@@ -5,5 +5,9 @@ defmodule SecretEntranceTest do
     test "advances the position if turning right" do
       assert SecretEntrance.turn(10, "R32") == 42
     end
+
+    test "retards the position if turning left" do
+      assert SecretEntrance.turn(42, "L32") == 10
+    end
   end
 end
