6ac4211fc939066b3b44cbdd64984f6ebf8fea09
Kerry Buckley <kerryjbuckley@gmail.com>
2025-12-01 09:57:28 +0000

    Count turns to the right

--- 01_secret_entrance/lib/secret_entrance.ex
+++ 01_secret_entrance/lib/secret_entrance.ex
@@ -1,2 +1,5 @@
 defmodule SecretEntrance do
+  def turn(position, <<"R", count::binary>>) do
+    position + String.to_integer(count)
+  end
 end
--- 01_secret_entrance/test/secret_entrance_test.exs
+++ 01_secret_entrance/test/secret_entrance_test.exs
@@ -1,3 +1,9 @@
 defmodule SecretEntranceTest do
   use ExUnit.Case, async: true
+
+  describe "SecretEntrance.turn/2" do
+    test "advances the position if turning right" do
+      assert SecretEntrance.turn(10, "R32") == 42
+    end
+  end
 end
