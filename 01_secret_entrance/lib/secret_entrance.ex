287a155cf1a76fd652f0de64cab5da35f2dce1c7
Kerry Buckley <kerryjbuckley@gmail.com>
2025-12-01 10:00:57 +0000

    Extract function

--- 01_secret_entrance/lib/secret_entrance.ex
+++ 01_secret_entrance/lib/secret_entrance.ex
@@ -1,9 +1,13 @@
 defmodule SecretEntrance do
   def turn(position, <<"R", count::binary>>) do
-    position + String.to_integer(count)
+    new_position(position, count, 1)
   end
 
   def turn(position, <<"L", count::binary>>) do
-    position - String.to_integer(count)
+    new_position(position, count, -1)
+  end
+
+  defp new_position(position, count, direction) do
+    position + String.to_integer(count) * direction
   end
 end
