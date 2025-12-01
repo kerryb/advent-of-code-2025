0b202fda91b7e513af1f9f84fc2a879728348d3c
Kerry Buckley <kerryjbuckley@gmail.com>
2025-12-01 19:26:58 +0000

    Fix edge case

--- 01_secret_entrance/lib/secret_entrance_2.ex
+++ 01_secret_entrance/lib/secret_entrance_2.ex
@@ -9,10 +9,10 @@ defmodule SecretEntrance2 do
 
   defp new_position(position, total, distance, direction) do
     new_position = position + String.to_integer(distance) * direction
-    {Integer.mod(new_position, 100), total + zero_count(new_position)}
+    {Integer.mod(new_position, 100), total + zero_count(position, new_position)}
   end
 
-  defp zero_count(0), do: 1
-  defp zero_count(position) when position > 0, do: div(position, 100)
-  defp zero_count(position), do: div(abs(position), 100) + 1
+  defp zero_count(_position, new_position) when new_position >= 0, do: div(new_position, 100)
+  defp zero_count(0, new_position), do: div(abs(new_position), 100)
+  defp zero_count(_position, new_position), do: div(abs(new_position), 100) + 1
 end
--- 01_secret_entrance/test/secret_entrance_2_test.exs
+++ 01_secret_entrance/test/secret_entrance_2_test.exs
@@ -14,6 +14,10 @@ defmodule SecretEntrance2Test do
       assert SecretEntrance2.turn(90, 0, "R20") == {10, 1}
     end
 
+    test "wraps from 0 to 99, incrementing the total, when turning left" do
+      assert SecretEntrance2.turn(10, 0, "L20") == {90, 1}
+    end
+
     test "increments the total when landing on zero" do
       assert SecretEntrance2.turn(90, 0, "R10") == {0, 1}
     end
@@ -24,6 +28,22 @@ defmodule SecretEntrance2Test do
 
     test "increments the total once per revolution when turning left by more than 100" do
       assert SecretEntrance2.turn(10, 0, "L220") == {90, 3}
+    end
+
+    test "increments the total correctly when turning right by more than 100 and ending on zero" do
+      assert SecretEntrance2.turn(80, 0, "R220") == {0, 3}
+    end
+
+    test "increments the total correctly when turning left by more than 100 and ending on zero" do
+      assert SecretEntrance2.turn(20, 0, "L220") == {0, 3}
+    end
+
+    test "increments the total correctly when turning right by 100 from zero to zero" do
+      assert SecretEntrance2.turn(0, 0, "R100") == {0, 1}
+    end
+
+    test "increments the total correctly when turning left by 100 from zero to zero" do
+      assert SecretEntrance2.turn(0, 0, "L100") == {0, 1}
     end
   end
 end
