ae36f699d8852bb103fa5309171638596a9ac8ef
Kerry Buckley <kerryjbuckley@gmail.com>
2025-12-01 19:37:44 +0000

    Put it all together again

--- 01_secret_entrance/lib/secret_entrance_2.ex
+++ 01_secret_entrance/lib/secret_entrance_2.ex
@@ -1,4 +1,13 @@
 defmodule SecretEntrance2 do
+  def decode(input) do
+    input
+    |> String.split()
+    |> Enum.reduce({50, 0}, fn instruction, {position, total} ->
+      turn(position, total, instruction)
+    end)
+    |> then(&elem(&1, 1))
+  end
+
   def turn(position, total, <<"R", distance::binary>>) do
     new_position(position, total, distance, 1)
   end
