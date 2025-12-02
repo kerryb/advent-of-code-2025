4bc76d309ed458a719168e10b57c0399e22bcb28
Kerry Buckley <kerryjbuckley@gmail.com>
2025-12-02 11:42:17 +0000

    Naive brute-force solution for day 2 part 1

--- 02_gift_shop/lib/gift_shop.ex
+++ 02_gift_shop/lib/gift_shop.ex
@@ -1,2 +1,24 @@
 defmodule GiftShop do
+  def run(input) do
+    input
+    |> ranges()
+    |> Enum.flat_map(fn range -> Enum.filter(range, &invalid?/1) end)
+    |> Enum.sum()
+  end
+
+  def ranges(input) do
+    input |> String.split(",") |> Enum.map(&range(&1))
+  end
+
+  defp range(string) do
+    string
+    |> String.split("-")
+    |> Enum.map(&String.to_integer/1)
+    |> then(fn [first, last] -> first..last end)
+  end
+
+  def invalid?(number) do
+    string = to_string(number)
+    match?({a, a}, String.split_at(string, div(String.length(string), 2)))
+  end
 end
--- 02_gift_shop/test/gift_shop_test.exs
+++ 02_gift_shop/test/gift_shop_test.exs
@@ -1,3 +1,36 @@
 defmodule GiftShopTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
+
+  describe "GiftShop.run/1" do
+    test "returns the total of all the invalid IDs" do
+      assert GiftShop.run(
+               "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
+             ) == 1_227_775_554
+    end
+  end
+
+  describe "GiftShop.ranges/1" do
+    test "extracts ranges from the input" do
+      assert GiftShop.ranges("11-22,95-115,998-1012,1188511880-1188511890") == [
+               11..22,
+               95..115,
+               998..1012,
+               1_188_511_880..1_188_511_890
+             ]
+    end
+  end
+
+  describe "GiftShop.invalid?/1" do
+    test "returns true for a repeated digit" do
+      assert GiftShop.invalid?("55")
+    end
+
+    test "returns true for a repeated multi-digit number" do
+      assert GiftShop.invalid?("123123")
+    end
+
+    test "returns false for a non-repetitive number" do
+      refute GiftShop.invalid?("12321")
+    end
+  end
 end
