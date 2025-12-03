58cfa5a889803aea83b970ad1a21742b4265cdf4
Kerry Buckley <kerryjbuckley@gmail.com>
2025-12-03 12:57:10 +0000

    Solve day 3 part 1

--- 03_lobby/lib/lobby.ex
+++ 03_lobby/lib/lobby.ex
@@ -1,2 +1,28 @@
 defmodule Lobby do
+  def total(input) do
+    input
+    |> String.split()
+    |> Enum.reduce(0, fn str, acc -> acc + max_pair(str) end)
+  end
+
+  def max_pair(string), do: max_pair(string, {0, 0})
+
+  defp max_pair(<<char::binary-size(1)>>, {a, b}) do
+    digit = String.to_integer(char)
+    {a, b} = if digit > b, do: {a, digit}, else: {a, b}
+    a * 10 + b
+  end
+
+  defp max_pair(<<char::binary-size(1), rest::binary>>, {a, b}) do
+    digit = String.to_integer(char)
+
+    acc =
+      cond do
+        digit > a -> {digit, 0}
+        digit > b -> {a, digit}
+        true -> {a, b}
+      end
+
+    max_pair(rest, acc)
+  end
 end
--- 03_lobby/test/lobby_test.exs
+++ 03_lobby/test/lobby_test.exs
@@ -1,3 +1,28 @@
 defmodule LobbyTest do
   use ExUnit.Case, async: true
+
+  describe "Lobby.total/1" do
+    test "returns the total joltage from the input" do
+      assert Lobby.total("""
+             987654321111111
+             811111111111119
+             234234234234278
+             818181911112111
+             """) == 357
+    end
+  end
+
+  describe "Lobby.max_pair/1" do
+    test "returns the number formed by the highest two digits when they are adjacent at the start" do
+      assert Lobby.max_pair("987654321111111") == 98
+    end
+
+    test "returns the number formed by the highest two digits when they are adjacent at the end" do
+      assert Lobby.max_pair("234234234234278") == 78
+    end
+
+    test "returns the number formed by the highest two digits when they are not adjacent" do
+      assert Lobby.max_pair("818181911112111") == 92
+    end
+  end
 end
