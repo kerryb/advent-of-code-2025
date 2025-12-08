8055662f9dcd1db41666b07c734744665fc474ae
Kerry Buckley <kerryjbuckley@gmail.com>
2025-12-08 21:43:53 +0000

    Complete day 3 part 1

--- 04_printing_department/lib/printing_department.ex
+++ 04_printing_department/lib/printing_department.ex
@@ -1,4 +1,14 @@
 defmodule PrintingDepartment do
+  def run(input) do
+    input
+    |> parse()
+    |> count_neighbours()
+    |> Enum.reduce(0, fn
+      {_, count}, acc when count < 4 -> acc + 1
+      _, acc -> acc
+    end)
+  end
+
   def parse(input) do
     input |> String.split() |> Enum.with_index() |> Enum.flat_map(&parse_row/1)
   end
@@ -9,4 +19,25 @@ defmodule PrintingDepartment do
 
   defp parse_cell({"@", x}, y), do: [{x, y}]
   defp parse_cell(_, _), do: []
+
+  def count_neighbours(coordinate_list) do
+    coordinate_list |> Map.new(&{&1, count_neighbours(&1, coordinate_list)})
+  end
+
+  defp count_neighbours(coordinates, coordinate_list) do
+    Enum.count(neighbours(coordinates), &(&1 in coordinate_list))
+  end
+
+  def neighbours({x, y}) do
+    [
+      {x - 1, y - 1},
+      {x - 1, y},
+      {x - 1, y + 1},
+      {x, y - 1},
+      {x, y + 1},
+      {x + 1, y - 1},
+      {x + 1, y},
+      {x + 1, y + 1}
+    ]
+  end
 end
--- 04_printing_department/test/printing_department_test.exs
+++ 04_printing_department/test/printing_department_test.exs
@@ -1,6 +1,23 @@
 defmodule PrintingDepartmentTest do
   use ExUnit.Case
 
+  describe "PrintingDepartment.run/1" do
+    test "returns the number of rolls with fewer than four neighbours" do
+      assert PrintingDepartment.run("""
+             ..@@.@@@@.
+             @@@.@.@.@@
+             @@@@@.@.@@
+             @.@@@@..@.
+             @@.@@@@.@@
+             .@@@@@@@.@
+             .@.@.@.@@@
+             @.@@@.@@@@
+             .@@@@@@@@.
+             @.@.@@@.@.
+             """) == 13
+    end
+  end
+
   describe "PrintingDepartment.parse/1" do
     test "returns the {x, y} coordinates of all rolls" do
       assert PrintingDepartment.parse("""
@@ -8,6 +25,32 @@ defmodule PrintingDepartmentTest do
              @.@
              ..@
              """) == [{1, 0}, {0, 1}, {2, 1}, {2, 2}]
+    end
+  end
+
+  describe "PrintingDepartment.neighbours/1" do
+    test "returns the coordinates of all eight potential neighbours" do
+      assert PrintingDepartment.neighbours({2, 3}) == [
+               {1, 2},
+               {1, 3},
+               {1, 4},
+               {2, 2},
+               {2, 4},
+               {3, 2},
+               {3, 3},
+               {3, 4}
+             ]
+    end
+  end
+
+  describe "PrintingDepartment.count_neighbours/1" do
+    test "returns a map of coordinates to neighbour counts" do
+      assert PrintingDepartment.count_neighbours([{1, 0}, {0, 1}, {2, 1}, {2, 2}]) == %{
+               {1, 0} => 2,
+               {0, 1} => 1,
+               {2, 1} => 2,
+               {2, 2} => 1
+             }
     end
   end
 end
