c36c3a07b51e64a23f8cad66da565ed0b3bf3bdd
Kerry Buckley <kerryjbuckley@gmail.com>
2025-12-08 21:22:31 +0000

    Parse input into a list of roll coordinates

--- 04_printing_department/lib/printing_department.ex
+++ 04_printing_department/lib/printing_department.ex
@@ -1,2 +1,12 @@
 defmodule PrintingDepartment do
+  def parse(input) do
+    input |> String.split() |> Enum.with_index() |> Enum.flat_map(&parse_row/1)
+  end
+
+  defp parse_row({row, y}) do
+    row |> String.codepoints() |> Enum.with_index() |> Enum.flat_map(&parse_cell(&1, y))
+  end
+
+  defp parse_cell({"@", x}, y), do: [{x, y}]
+  defp parse_cell(_, _), do: []
 end
--- 04_printing_department/test/printing_department_test.exs
+++ 04_printing_department/test/printing_department_test.exs
@@ -1,3 +1,13 @@
 defmodule PrintingDepartmentTest do
   use ExUnit.Case
+
+  describe "PrintingDepartment.parse/1" do
+    test "returns the {x, y} coordinates of all rolls" do
+      assert PrintingDepartment.parse("""
+             .@.
+             @.@
+             ..@
+             """) == [{1, 0}, {0, 1}, {2, 1}, {2, 2}]
+    end
+  end
 end
