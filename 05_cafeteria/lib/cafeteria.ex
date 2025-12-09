f4d45879d666948056621e1a21078a7acceaa9ec
Kerry Buckley <kerryjbuckley@gmail.com>
2025-12-09 16:27:49 +0000

    Complete day 5 part 1

--- 05_cafeteria/lib/cafeteria.ex
+++ 05_cafeteria/lib/cafeteria.ex
@@ -1,3 +1,28 @@
 defmodule Cafeteria do
   @moduledoc false
+
+  defstruct [:ranges, :ids]
+
+  def count_fresh(input) do
+    data = parse(input)
+    Enum.count(data.ids, fn id -> Enum.any?(data.ranges, &(id in &1)) end)
+  end
+
+  def parse(input) do
+    [ranges, ids] = String.split(input, ~r/\n{2,}/)
+    %__MODULE__{ranges: parse_ranges(ranges), ids: parse_ids(ids)}
+  end
+
+  defp parse_ranges(input) do
+    input |> String.split() |> Enum.map(&parse_range/1)
+  end
+
+  defp parse_range(line) do
+    [start, finish] = line |> String.split("-") |> Enum.map(&String.to_integer/1)
+    start..finish
+  end
+
+  defp parse_ids(input) do
+    input |> String.split() |> Enum.map(&String.to_integer/1)
+  end
 end
--- 05_cafeteria/test/cafeteria_test.exs
+++ 05_cafeteria/test/cafeteria_test.exs
@@ -1,3 +1,39 @@
 defmodule CafeteriaTest do
   use ExUnit.Case
+
+  describe "Cafeteria.count_fresh/1" do
+    test "returns the number of IDs that fall within a range" do
+      assert Cafeteria.count_fresh("""
+             3-5
+             10-14
+             16-20
+             12-18
+
+             1
+             5
+             8
+             11
+             17
+             32
+             """) == 3
+    end
+  end
+
+  describe "Cafeteria.parse/1" do
+    test "returns the ranges and ids" do
+      assert Cafeteria.parse("""
+             3-5
+             10-14
+             16-20
+             12-18
+
+             1
+             5
+             8
+             11
+             17
+             32
+             """) == %Cafeteria{ranges: [3..5, 10..14, 16..20, 12..18], ids: [1, 5, 8, 11, 17, 32]}
+    end
+  end
 end
