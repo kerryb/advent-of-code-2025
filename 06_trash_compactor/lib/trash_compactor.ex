771987ed68c87700c1d6c1601dcdea3eec0954dd
Kerry Buckley <kerryjbuckley@gmail.com>
2025-12-10 13:34:24 +0000

    Solve day 6 part 2

--- 06_trash_compactor/lib/trash_compactor.ex
+++ 06_trash_compactor/lib/trash_compactor.ex
@@ -15,6 +15,49 @@ defmodule TrashCompactor do
     calculate([operation_tail | number_tails], sum + result)
   end
 
+  def run_2(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.reverse()
+    |> Enum.map(&String.codepoints/1)
+    |> calculate_2([], nil, 0)
+  end
+
+  defp calculate_2([[] | _], numbers, operation, sum) do
+    sum + perform_calculation(numbers, operation)
+  end
+
+  defp calculate_2([operations | lines], numbers, operation, sum) do
+    lists = [tl(operations) | Enum.map(lines, &tl/1)]
+
+    new_operation =
+      case hd(operations) do
+        " " -> operation
+        op -> op
+      end
+
+    case number_from_column(lines) do
+      nil -> calculate_2(lists, [], nil, sum + perform_calculation(numbers, operation))
+      number -> calculate_2(lists, [number | numbers], new_operation, sum)
+    end
+  end
+
+  defp number_from_column(lines) do
+    case lines
+         |> Enum.flat_map(fn
+           [" " | _] -> []
+           [digit | _] -> [digit]
+         end)
+         |> Enum.join() do
+      "" -> nil
+      number -> number |> String.reverse() |> String.to_integer()
+    end
+  end
+
+  defp perform_calculation(numbers, operation) do
+    Enum.reduce(numbers, operation_fun(operation))
+  end
+
   defp operation_fun("+"), do: &+/2
   defp operation_fun("*"), do: &*/2
 end
--- 06_trash_compactor/test/trash_compactor_test.exs
+++ 06_trash_compactor/test/trash_compactor_test.exs
@@ -11,4 +11,15 @@ defmodule TrashCompactorTest do
              """) == 4_277_556
     end
   end
+
+  describe "TrashCompactor.run_2/1" do
+    test "returns the sum of all the results, reading numbers vertically" do
+      assert TrashCompactor.run_2("""
+             123 328  51 64 
+              45 64  387 23 
+               6 98  215 314
+             *   +   *   +  
+             """) == 3_263_827
+    end
+  end
 end
