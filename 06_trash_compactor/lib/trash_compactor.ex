3ee99a89a8204437da6d817c288ed0c2b4425428
Kerry Buckley <kerryjbuckley@gmail.com>
2025-12-10 07:41:43 +0000

    Complete day 6 part 1

--- 06_trash_compactor/lib/trash_compactor.ex
+++ 06_trash_compactor/lib/trash_compactor.ex
@@ -1,3 +1,20 @@
 defmodule TrashCompactor do
   @moduledoc false
+
+  def run(input) do
+    input |> String.split("\n", trim: true) |> Enum.reverse() |> Enum.map(&String.split/1) |> calculate(0)
+  end
+
+  defp calculate([[] | _], sum), do: sum
+
+  defp calculate([operations | numbers], sum) do
+    [operation | operation_tail] = operations
+    number_heads = Enum.map(numbers, &hd/1)
+    number_tails = Enum.map(numbers, &tl/1)
+    result = number_heads |> Enum.map(&String.to_integer/1) |> Enum.reduce(operation_fun(operation))
+    calculate([operation_tail | number_tails], sum + result)
+  end
+
+  defp operation_fun("+"), do: &+/2
+  defp operation_fun("*"), do: &*/2
 end
--- 06_trash_compactor/test/trash_compactor_test.exs
+++ 06_trash_compactor/test/trash_compactor_test.exs
@@ -1,3 +1,14 @@
 defmodule TrashCompactorTest do
   use ExUnit.Case
+
+  describe "TrashCompactor.run/1" do
+    test "returns the sum of all the results" do
+      assert TrashCompactor.run("""
+             123 328  51 64 
+              45 64  387 23 
+               6 98  215 314
+             *   +   *   +  
+             """) == 4_277_556
+    end
+  end
 end
