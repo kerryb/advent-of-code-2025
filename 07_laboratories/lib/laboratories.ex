f7879b63282cb735c736795da03b66bba47b22e1
Kerry Buckley <kerryjbuckley@gmail.com>
2025-12-11 13:01:48 +0000

    Rewrite day 7 part 2 to run pretty much instantaneously

--- 07_laboratories/lib/laboratories.ex
+++ 07_laboratories/lib/laboratories.ex
@@ -2,7 +2,7 @@ defmodule Laboratories do
   @moduledoc false
 
   def run(input), do: run(parse(input), [], 0)
-  def run_2(input), do: run_2(parse(input), nil)
+  def run_2(input), do: run_2(parse(input), [])
 
   defp parse(input) do
     input |> String.split("\n", trim: true) |> Enum.map(&String.codepoints/1)
@@ -13,10 +13,36 @@ defmodule Laboratories do
 
   defp run([head | tail], beams, count) do
     splitters = splitters(head, beams)
-
     beams = next_beams(beams, splitters)
-
     run(tail, beams, count + length(splitters))
+  end
+
+  defp next_beams(beams, splitters) do
+    beams
+    |> Enum.flat_map(fn beam ->
+      if beam in splitters, do: [beam - 1, beam + 1], else: [beam]
+    end)
+    |> Enum.uniq()
+  end
+
+  defp run_2([head | tail], []), do: run_2(tail, %{start(head) => 1})
+  defp run_2([], frequencies), do: frequencies |> Map.values() |> Enum.sum()
+
+  defp run_2([head | tail], frequencies) do
+    splitters = splitters(head, Map.keys(frequencies))
+    new_paths = Enum.flat_map(splitters, &%{(&1 - 1) => frequencies[&1], (&1 + 1) => frequencies[&1]})
+    new_frequencies = frequencies |> increment_new_paths(new_paths) |> remove_old_paths(splitters)
+    run_2(tail, new_frequencies)
+  end
+
+  defp increment_new_paths(frequencies, paths) do
+    Enum.reduce(paths, frequencies, fn {path, count}, frequencies ->
+      Map.update(frequencies, path, count, &(&1 + count))
+    end)
+  end
+
+  defp remove_old_paths(frequencies, paths) do
+    Enum.reduce(paths, frequencies, fn path, frequencies -> Map.put(frequencies, path, 0) end)
   end
 
   defp splitters(line, beams) do
@@ -29,25 +55,6 @@ defmodule Laboratories do
       _ ->
         []
     end)
-  end
-
-  defp next_beams(beams, splitters) do
-    beams
-    |> Enum.flat_map(fn beam ->
-      if beam in splitters, do: [beam - 1, beam + 1], else: [beam]
-    end)
-    |> Enum.uniq()
-  end
-
-  defp run_2([head | tail], nil), do: run_2(tail, start(head))
-  defp run_2([], _beam), do: 1
-
-  defp run_2([head | tail], beam) do
-    if Enum.at(head, beam) == "^" do
-      run_2(tail, beam - 1) + run_2(tail, beam + 1)
-    else
-      run_2(tail, beam)
-    end
   end
 
   defp start(line) do
