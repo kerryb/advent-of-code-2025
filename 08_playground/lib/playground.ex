ed7c5a0d9580061189f21c8de2ee6a625952f1a9
Kerry Buckley <kerryjbuckley@gmail.com>
2025-12-12 12:07:48 +0000

    Complete day 8 part 2

--- 08_playground/lib/playground.ex
+++ 08_playground/lib/playground.ex
@@ -2,25 +2,46 @@ defmodule Playground do
   @moduledoc false
 
   def run(input, count) do
-    {_, circuits} =
-      Enum.reduce(1..count, {distances(input), []}, fn _, {distances, circuits} ->
+    {_, _point_1, _point_2, circuits} =
+      Enum.reduce(1..count, {input |> points() |> distances(), nil, nil, []}, fn _,
+                                                                                 {distances, _point_1, _point_2, circuits} ->
         connect_shortest(distances, circuits)
       end)
 
     circuits |> Enum.map(&length/1) |> Enum.sort(:desc) |> Enum.take(3) |> Enum.reduce(&*/2)
   end
 
-  def distances(input) do
-    points =
-      input
-      |> String.split("\n", trim: true)
-      |> Enum.map(fn line -> line |> String.split(",") |> Enum.map(&String.to_integer/1) end)
+  def run_2(input) do
+    points = points(input)
+    distances = distances(points)
+    count = length(points)
+    {{x, _y, _z}, {xx, _yy, _zz}} = connect_all(distances, [], count)
+    x * xx
+  end
 
+  defp connect_all(distances, circuits, count) do
+    {new_distances, point_1, point_2, new_circuits} = connect_shortest(distances, circuits)
+
+    if length(new_circuits) == 1 and length(hd(new_circuits)) == count do
+      {point_1, point_2}
+    else
+      connect_all(new_distances, new_circuits, count)
+    end
+  end
+
+  def distances(points) do
     Map.new(
-      for [a, b, c] = first <- points, [aa, bb, cc] = second <- points, first < second do
+      for {a, b, c} = first <- points, {aa, bb, cc} = second <- points, first < second do
         {{{a, b, c}, {aa, bb, cc}}, :math.sqrt((a - aa) ** 2 + (b - bb) ** 2 + (c - cc) ** 2)}
       end
     )
+  end
+
+  def points(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line -> line |> String.split(",") |> Enum.map(&String.to_integer/1) end)
+    |> Enum.map(fn [a, b, c] -> {a, b, c} end)
   end
 
   def connect_shortest(distances, circuits) do
@@ -45,6 +66,6 @@ defmodule Playground do
           circuits |> List.update_at(b_index, &(Enum.at(circuits, a_index) ++ &1)) |> List.delete_at(a_index)
       end
 
-    {distances, circuits}
+    {distances, a, b, circuits}
   end
 end
--- 08_playground/test/playground_test.exs
+++ 08_playground/test/playground_test.exs
@@ -31,31 +31,45 @@ defmodule PlaygroundTest do
     end
   end
 
+  describe "Playground.run_2/1" do
+    test "multiplies the X coordinates of the last two junction boxes that need to be connected" do
+      assert Playground.run_2("""
+             162,817,812
+             57,618,57
+             906,360,560
+             592,479,940
+             352,342,300
+             466,668,158
+             542,29,236
+             431,825,988
+             739,650,466
+             52,470,668
+             216,146,977
+             819,987,18
+             117,168,530
+             805,96,715
+             346,949,466
+             970,615,88
+             941,993,340
+             862,61,35
+             984,92,344
+             425,690,689
+             """) == 25_272
+    end
+  end
+
+  describe "Playground.points/1" do
+    test "extracts the points from the input" do
+      assert Playground.points("""
+             162,817,812
+             57,618,57
+             """) == [{162, 817, 812}, {57, 618, 57}]
+    end
+  end
+
   describe "Playground.distances/1" do
     test "calculates the distance between each pair of points" do
-      distances =
-        Playground.distances("""
-        162,817,812
-        57,618,57
-        906,360,560
-        592,479,940
-        352,342,300
-        466,668,158
-        542,29,236
-        431,825,988
-        739,650,466
-        52,470,668
-        216,146,977
-        819,987,18
-        117,168,530
-        805,96,715
-        346,949,466
-        970,615,88
-        941,993,340
-        862,61,35
-        984,92,344
-        425,690,689
-        """)
+      distances = Playground.distances([{162, 817, 812}, {57, 618, 57}, {431, 825, 988}, {739, 650, 466}])
 
       assert_in_delta distances[{{162, 817, 812}, {431, 825, 988}}],
                       :math.sqrt((431 - 162) ** 2 + (825 - 817) ** 2 + (988 - 812) ** 2),
@@ -65,7 +79,7 @@ defmodule PlaygroundTest do
 
   describe "Playground.connect_shortest/2" do
     test "removes the shortest pair from the distances map" do
-      {distances, _circuits} =
+      {distances, _point_1, _point_2, _circuits} =
         Playground.connect_shortest(
           %{
             {{1, 2, 3}, {3, 4, 5}} => 3.4,
@@ -78,8 +92,20 @@ defmodule PlaygroundTest do
       refute {{1, 2, 3}, {2, 3, 4}} in Map.keys(distances)
     end
 
+    test "returns the points that were connected" do
+      assert {_distances, {1, 2, 3}, {2, 3, 4}, _circuits} =
+               Playground.connect_shortest(
+                 %{
+                   {{1, 2, 3}, {3, 4, 5}} => 3.4,
+                   {{1, 2, 3}, {2, 3, 4}} => 1.2,
+                   {{1, 2, 3}, {4, 5, 6}} => 5.6
+                 },
+                 []
+               )
+    end
+
     test "returns a new circuit for the first pair" do
-      assert {_distances, [[{1, 2, 3}, {2, 3, 4}]]} =
+      assert {_distances, _point_1, _point_2, [[{1, 2, 3}, {2, 3, 4}]]} =
                Playground.connect_shortest(
                  %{
                    {{1, 2, 3}, {3, 4, 5}} => 3.4,
@@ -91,7 +117,7 @@ defmodule PlaygroundTest do
     end
 
     test "adds a new circuit if neither of the pair are already in a circuit" do
-      assert {_distances, [[{1, 2, 4}, {3, 4, 5}], [{1, 2, 3}, {2, 3, 4}]]} =
+      assert {_distances, _point_1, _point_2, [[{1, 2, 4}, {3, 4, 5}], [{1, 2, 3}, {2, 3, 4}]]} =
                Playground.connect_shortest(
                  %{
                    {{1, 2, 4}, {3, 4, 5}} => 3.4,
@@ -102,7 +128,7 @@ defmodule PlaygroundTest do
     end
 
     test "adds the second point to an existing circuit if the first point is already in it" do
-      assert {_distances, [[{3, 4, 5}, {1, 2, 3}, {2, 3, 4}]]} =
+      assert {_distances, _point_1, _point_2, [[{3, 4, 5}, {1, 2, 3}, {2, 3, 4}]]} =
                Playground.connect_shortest(
                  %{
                    {{1, 2, 3}, {3, 4, 5}} => 3.4,
@@ -113,7 +139,7 @@ defmodule PlaygroundTest do
     end
 
     test "adds the first point to an existing circuit if the second point is already in it" do
-      assert {_distances, [[{3, 4, 5}, {1, 2, 3}, {2, 3, 4}]]} =
+      assert {_distances, _point_1, _point_2, [[{3, 4, 5}, {1, 2, 3}, {2, 3, 4}]]} =
                Playground.connect_shortest(
                  %{
                    {{2, 3, 4}, {3, 4, 5}} => 3.4,
@@ -124,7 +150,7 @@ defmodule PlaygroundTest do
     end
 
     test "combines two circuits if one point is in each" do
-      assert {_distances, [[{1, 2, 3}, {2, 3, 4}, {3, 4, 5}, {7, 8, 9}]]} =
+      assert {_distances, _point_1, _point_2, [[{1, 2, 3}, {2, 3, 4}, {3, 4, 5}, {7, 8, 9}]]} =
                Playground.connect_shortest(
                  %{
                    {{2, 3, 4}, {3, 4, 5}} => 3.4,
@@ -135,7 +161,7 @@ defmodule PlaygroundTest do
     end
 
     test "does not change any circuits if both points are already in the same one" do
-      assert {_distances, [[{1, 2, 3}, {2, 3, 4}, {3, 4, 5}, {7, 8, 9}], [{3, 2, 1}, {5, 4, 3}]]} =
+      assert {_distances, _point_1, _point_2, [[{1, 2, 3}, {2, 3, 4}, {3, 4, 5}, {7, 8, 9}], [{3, 2, 1}, {5, 4, 3}]]} =
                Playground.connect_shortest(
                  %{
                    {{2, 3, 4}, {3, 4, 5}} => 3.4,
