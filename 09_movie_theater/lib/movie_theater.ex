67d684eeeb5a3b91af899432ffe4a5453a134a4a
Kerry Buckley <kerryjbuckley@gmail.com>
2025-12-12 15:21:58 +0000

    Complete day 9 part 1

--- 09_movie_theater/lib/movie_theater.ex
+++ 09_movie_theater/lib/movie_theater.ex
@@ -1,3 +1,26 @@
 defmodule MovieTheater do
   @moduledoc false
+
+  def run(input), do: input |> parse() |> largest_area()
+
+  def parse(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line -> String.split(line, ",") end)
+    |> Enum.map(fn [a, b] -> {String.to_integer(a), String.to_integer(b)} end)
+  end
+
+  def largest_area(points) do
+    {a, b} =
+      Enum.max_by(
+        for a <- points, b <- points, a > b do
+          {a, b}
+        end,
+        fn {a, b} -> area(a, b) end
+      )
+
+    area(a, b)
+  end
+
+  defp area({x, y}, {xx, yy}), do: (abs(x - xx) + 1) * (abs(y - yy) + 1)
 end
--- 09_movie_theater/test/movie_theater_test.exs
+++ 09_movie_theater/test/movie_theater_test.exs
@@ -1,3 +1,19 @@
 defmodule MovieTheaterTest do
   use ExUnit.Case, async: true
+
+  describe "MovieTheater.largest_area/1" do
+    test "returnas the largest area between any pair of points" do
+      assert MovieTheater.largest_area([{7, 1}, {11, 1}, {11, 7}, {9, 7}, {9, 5}, {2, 5}, {2, 3}, {7, 3}]) == 50
+    end
+  end
+
+  describe "MovieTheater.parse/1" do
+    test "extracts the points from the input" do
+      assert MovieTheater.parse("""
+             7,1
+             11,1
+             11,7
+             """) == [{7, 1}, {11, 1}, {11, 7}]
+    end
+  end
 end
